f0f6ca6f8ccd1bdd3ebd86001350ded28fdcb84a
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-09-01 11:53:10 +0200

    add warning message

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 background from 
 
 Dung Tran (modified by the algorithm) https://pixabay.com/users/kollsd-14736411/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4922621
+
+Arrow svg from (https://www.flaticon.com/authors/freepik)
--- src/App.tsx
+++ src/App.tsx
@@ -132,6 +132,14 @@ function App() {
       <div className="container mx-auto flex flex-col gap-10 bg-neutral">
         <NavBar/>
         <div className="flex flex-col px-4 flex flex-col gap-5" >
+          <div className="alert alert-warning">
+            <div className="flex-1">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-6 h-6 mx-2 stroke-current"> 
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>
+              </svg> 
+              <label>The algorithm is resource intensive. So it may not finish on mobile phone or low configuration.</label>
+            </div>
+          </div>
             {
               !openCVLoaded ?
               (<div className="flex flex-col items-center">
