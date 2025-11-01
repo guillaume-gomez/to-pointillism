636f939aac8ae5db52e2658655afbcff1c3c3ea8
Guillaume Gomez <guillaume.gomez846@gmail.com>
2025-11-01 13:57:46 +0100

    add checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    checker({ typescript: { tsconfigPath: "./tsconfig.app.json" } }),
     react(),
   ],
 })
